347b9d6779d9ad109b6e1c3450e96a1a4c241bd8
quyennguyencoder <quyen0012341982@gmail.com>
2026-07-09 08:46:04 +0700

    first commit

---
+++ src/test/java/com/nguyenquyen/mockproject_062026_group3/MockProject062026Group3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nguyenquyen.mockproject_062026_group3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockProject062026Group3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
